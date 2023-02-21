ae40cdabf9c3d855eb75ceb552acc3d2d5323163
Toteya <111172685+Toteya@users.noreply.github.com>
2023-02-21 10:26:58 +0000

    Print to stdout using _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+/* prototype */
+int _putchar(char c);
+
+/**
+ * main - Prints a string using _putchar function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
