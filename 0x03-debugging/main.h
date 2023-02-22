4b263d45343e564afe64be5e9f94e6f9c1050f61
Toteya <111172685+Toteya@users.noreply.github.com>
2023-02-22 13:54:17 +0000

    Task3: Debug faulty code, leap years

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Standard Libraries */
+#include <stdio.h>
+
+/* Function Prototypes */
+
 void positive_or_negative(int);
 int largest_number(int, int, int);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif /* MAIN_H */
