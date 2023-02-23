1082ea752184934fb6b05c6966dee5a9394bd7c9
Toteya <111172685+Toteya@users.noreply.github.com>
2023-02-23 19:24:22 +0000

    Task9: Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Fizz Buzz, 1 to 100, multiples of 3 and 5
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i)
+			printf(" ");
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Fizz Buzz");
+		else
+			printf("%d", i);
+	}
+	printf("\n");
+
+	return (0);
+}
