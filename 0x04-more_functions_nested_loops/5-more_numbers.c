a19ea7f9f35abea1092aa26630060a4d0da5ab75
Toteya <111172685+Toteya@users.noreply.github.com>
2023-02-23 17:32:59 +0000

    Task5: Print numbers ten times, fix output

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * more-numbers - Prints 
+ * more_numbers - Prints numbers 0 to 14 ten times
  *
  * Return: Nothing
  */
@@ -11,9 +11,9 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			if(j / 10)
+			if (j / 10)
 				_putchar(j / 10 + '0');
 			_putchar(j % 10 + '0');
 		}
