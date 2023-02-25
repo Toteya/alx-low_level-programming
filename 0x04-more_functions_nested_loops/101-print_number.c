29c5b564e65997ef19c62cd4cfabd41df91b16e4
Toteya <111172685+Toteya@users.noreply.github.com>
2023-02-25 01:05:28 +0000

    Task12: Print an integer with putchar, fix: int_min

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,22 +11,21 @@ void print_number(int n)
 {
 	int D = 1000000000;
 	int num;
-	int ready = 0;
+	int ready = 0, sign = 1;
 
 	if (n < 0)
 	{
-		n *= -1;
+		sign *= -1;
 		_putchar('-');
 	}
 	while (D > 0)
 	{
 		num = n / D;
-/*		printf("num: %d, n: %d, D: %d\n", num, n, D); */
 		if (num)
 			ready = 1;
 		if (ready || D <= 1)
 		{
-			_putchar(num + '0');
+			_putchar(sign * num + '0');
 			n = n % D;
 		}
 		D = D / 10;
