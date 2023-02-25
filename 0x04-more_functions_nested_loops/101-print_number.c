54dda7dbc9a85d159b0a90021c8113f86da3588a
Toteya <111172685+Toteya@users.noreply.github.com>
2023-02-25 00:48:05 +0000

    Task12: Print an integer with putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,18 +13,22 @@ void print_number(int n)
 	int num;
 	int ready = 0;
 
-	num = n;
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+	}
 	while (D > 0)
 	{
-		num = num / D;
-		printf("num: %d, n: %d, D: %d\n", num, n, D);
+		num = n / D;
+/*		printf("num: %d, n: %d, D: %d\n", num, n, D); */
 		if (num)
 			ready = 1;
-		if (ready)
+		if (ready || D <= 1)
 		{
 			_putchar(num + '0');
+			n = n % D;
 		}
-		num = n - n % D;
 		D = D / 10;
 	}
 }
