2f8ad1d5b1fd1050bd37c902c005c0bc1323b2c3
Toteya <111172685+Toteya@users.noreply.github.com>
2023-02-27 18:38:07 +0000

    Task10: String to Integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+/**
+ * _atoi - Converts a string to an integer
+ * @s: The string to be converted to an integer
+ *
+ * Return: The integer converted from the string
+ */
+int _atoi(char *s)
+{
+	int num = 0;
+	int i;
+	int sign = 1;
+	int numfound = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (!numfound)
+		{
+			if (s[i] == '-')
+				sign *= -1;
+			if (s[i] >= '0' && s[i] <= '9')
+				numfound = 1;
+		}
+		if (numfound)
+		{
+			if (!(s[i] >= '0' && s[i] <= '9'))
+				break;
+			num = num * 10 + s[i] - '0';
+		}
+	}
+	return (num * sign);
+}
