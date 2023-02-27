493da4ae48a37c4b2bcbeeebd35cf084315acad9
Toteya <111172685+Toteya@users.noreply.github.com>
2023-02-27 14:45:32 +0000

    Task6: Print every other character of a string, fix: infinite loop

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,8 +11,9 @@ void puts2(char *str)
 	while (*str != '\0')
 	{
 		_putchar(*str);
-			if (*(str + 1) != '\0')
-				str = str + 2;
+			if (*(str + 1) == '\0')
+				break;
+		str = str + 2;
 	}
 	_putchar('\n');
 }
