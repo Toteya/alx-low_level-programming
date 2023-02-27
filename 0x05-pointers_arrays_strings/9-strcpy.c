c4bb7728523d804cc02e3bcec83fc9769b3cae06
Toteya <111172685+Toteya@users.noreply.github.com>
2023-02-27 16:00:51 +0000

    Task9: Copy a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+/**
+ * _strcpy - Copies a string
+ * @dest: Pointer to a buffer to which to copy the string
+ * @src: Pointer to the string to be copid to dest
+ *
+ * Return: Pointer to the copied string, dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return dest;
+}
