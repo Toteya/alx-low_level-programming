3b91de2f773ee720ede0186a5f9c5b07bb3115f5
Toteya <111172685+Toteya@users.noreply.github.com>
2023-02-27 13:45:48 +0000

    Task4: Print a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse
+ * @s: String to be printed
+ *
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	for (; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
