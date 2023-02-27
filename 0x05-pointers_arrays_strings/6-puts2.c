cd13b74bfc5b442e1cfbea64051e77d9d583b50e
Toteya <111172685+Toteya@users.noreply.github.com>
2023-02-27 14:40:24 +0000

    Task6: Print every other character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string
+ * @str: The string to be printed
+ *
+ * Return: Nothing
+ */
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+			if (*(str + 1) != '\0')
+				str = str + 2;
+	}
+	_putchar('\n');
+}
