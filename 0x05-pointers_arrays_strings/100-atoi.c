5feab56c3b6f1e0de1e181726934a4e0e304226b
Toteya <111172685+Toteya@users.noreply.github.com>
2023-02-27 19:24:08 +0000

    Task10: Convert string to integer, fix bugs: INTMAX, INTMIN

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,15 +17,22 @@ int _atoi(char *s)
 		{
 			if (s[i] == '-')
 				sign *= -1;
-			if (s[i] >= '0' && s[i] <= '9')
+			if (s[i] >= '1' && s[i] <= '9')
+			{
 				numfound = 1;
+				if (sign < 0)
+					num *= -1;
+			}
 		}
 		if (numfound)
 		{
 			if (!(s[i] >= '0' && s[i] <= '9'))
 				break;
-			num = num * 10 + s[i] - '0';
+			if (sign > 0)
+				num = num * 10 + (s[i] - '0');
+			else
+				num = num * 10 - (s[i] - '0');
 		}
 	}
-	return (num * sign);
+	return (num);
 }
