c24b3e2c5851e5f0004efa1497022d1d5463784d
Toteya <111172685+Toteya@users.noreply.github.com>
2023-03-06 13:05:22 +0000

    Task2: Search for a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: The string in which to search for the given character
+ * @c: The character to search for in s
+ *
+ * Return: Pointer to the first occurrence of the character c
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
