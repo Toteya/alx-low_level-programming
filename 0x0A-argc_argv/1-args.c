a2fd5fa9bff947680a6eed2e6d936d6bc5132809
Toteya <111172685+Toteya@users.noreply.github.com>
2023-03-10 10:42:32 +0000

    Task1: Print number of command line arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Prints the number of command line arguments passed to the program
+ * @argc: Number of command line arguments
+ * @argv: String array of command line arguments
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
