ae2fb1b3d6c0fdc2cc69b7a01a7533c2131b2d62
Toteya <111172685+Toteya@users.noreply.github.com>
2023-03-10 10:36:29 +0000

    Task0: Print the name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Prints the name of the program
+ * @argc: Number of command line arguments to main
+ * @argv: Array of command line arguments
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Libraries */
+#include <stdio.h>
+
+/* Funtion prototypes */
+int _putchar(char c);
+
+#endif /* MAIN_H */
