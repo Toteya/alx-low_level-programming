084f51055d09fb78ce123087da9cc749ae604b6a
Toteya <111172685+Toteya@users.noreply.github.com>
2023-03-10 12:13:23 +0000

    Task4: Add numbers on the command line

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * is_numeric - Checks if a string is made up entirely of numeric characters
+ * @s: The string to be checked
+ *
+ * Return: 1 if s is entirely numeric. Otherwise return 0.
+ */
+int is_numeric(char *s)
+{
+	while (*s != '\0')
+	{
+		if (*s < '0' || *s > '9')
+			return (0);
+		s++;
+	}
+	return (1);
+}
+
+/**
  * main - Adds two numbers and prints the result
  * @argc: Number of arguments to main
  * @argv: Array of command line arguments to main
@@ -9,15 +26,18 @@
  */
 int main(int argc, char *argv[])
 {
-	int sum;
+	int sum = 0;
+	int i;
 
-	if (argc != 3)
+	for (i = 1; i < argc; i++)
 	{
-		printf("Error\n");
-		return (1);
+		if (!is_numeric(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
 	}
-
-	sum = atoi(argv[1]) + atoi(argv[2]);
 	printf("%d\n", sum);
 	return (0);
 }
