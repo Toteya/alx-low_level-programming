61bf065df1921b794b546760a5b3ff5d2c7049cc
Toteya <111172685+Toteya@users.noreply.github.com>
2023-03-13 14:00:49 +0000

    Task3: Alloc memeory to 2-dimensional array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * alloc_grid - Allocates memory to a 2-d array
+ * @width: The width of the array
+ * @height: The height of the array
+ *
+ * Return: A pointer to the 2-d array
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **grid;
+
+	if (!width || !height)
+		return (NULL);
+
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+			return (NULL);
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
