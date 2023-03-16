bcb1c5d13154247fc85e20c2b3d8c339cabffe45
Toteya <111172685+Toteya@users.noreply.github.com>
2023-03-16 18:13:26 +0000

    Task2: Calloc, fix: initialise array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,10 +9,19 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	unsigned int i;
 	void *ptr = NULL;
+
+	if (!nmemb || !size)
+		return (NULL);
 
 	ptr = malloc(nmemb * size);
 	if (!ptr)
 		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+	{
+		((char *)ptr)[i * size] = 0x00;
+	}
 	return (ptr);
 }
