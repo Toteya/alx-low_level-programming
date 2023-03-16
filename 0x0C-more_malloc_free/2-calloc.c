f4171639ee4ce41e858823b46a78f595f058bb03
Toteya <111172685+Toteya@users.noreply.github.com>
2023-03-16 19:07:24 +0000

    Task2: Calloc, fix: Trial and error

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	void *ptr = NULL;
 
-	if (!nmemb || !size)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	ptr = malloc(nmemb * size);
@@ -21,7 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	for (i = 0; i < nmemb; i++)
 	{
-		((char *)ptr)[i * size] = 0x00;
+		((char *)ptr)[i * size] = 0;
 	}
 	return (ptr);
 }
