6b9485f8685dd6ebc877c0cd92f82d7177b740de
Toteya <111172685+Toteya@users.noreply.github.com>
2023-03-16 15:11:32 +0000

    Task2: Calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _calloc - Allocates memory for an array using malloc
+ * @nmemb: The number of elements in the array
+ * @size: The size of each element
+ *
+ * Return: Pointer to the allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr = NULL;
+
+	ptr = malloc(nmemb * size);
+	return (ptr);
+}
