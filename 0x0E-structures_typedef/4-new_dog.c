7809a2fb328947ef4cd8103f3dbbf968acad47a4
Toteya <111172685+Toteya@users.noreply.github.com>
2023-03-20 15:15:30 +0000

    Task4: Create a new struct dog, fix: trial & error

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,12 +12,25 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog = NULL;
 
+	if (!name || !owner)
+		return (NULL);
 	dog = malloc(sizeof(dog_t));
 	if (!dog)
 		return (NULL);
 
 	dog->name = strdup(name);
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
 	dog->age = age;
 	dog->owner = strdup(owner);
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return(NULL);
+	}
 	return (dog);
 }
