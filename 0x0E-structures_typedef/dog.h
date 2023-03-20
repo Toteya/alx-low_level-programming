0f76f1859f73bf4d1feaeb9d74ce8fa00c8b97b7
Toteya <111172685+Toteya@users.noreply.github.com>
2023-03-20 13:25:11 +0000

    Task0: Data structure - dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Data structure defining a dog
+ * @name: The name (string) of the dog
+ * @age: The age (float) of the dog
+ * @owner: The owner (string) of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
