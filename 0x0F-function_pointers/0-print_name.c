e5642bb39871baa453233578a26f6690b2556bac
Toteya <111172685+Toteya@users.noreply.github.com>
2023-03-22 12:10:29 +0000

    Task0: Function that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print_name - Prints a name
+ * @name: String name to be printed
+ * @f: Pointer to a function that prints a name
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
