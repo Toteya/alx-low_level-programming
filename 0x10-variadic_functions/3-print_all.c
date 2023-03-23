97c62bf863cf47c9d022f9c34c4cac8efda960d5
Toteya <111172685+Toteya@users.noreply.github.com>
2023-03-23 20:20:17 +0000

    Task3: Print anything, bug fix: NULL-pointer format

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,42 +9,38 @@
 void print_all(const char * const format, ...)
 {
 	va_list ap;
-	int i = 0;
-	char f, *s;
+	int i = 0, skip = 0;
+	char *s;
 
-	while (format)
+	va_start(ap, format);
+	while (format && format[i] != '\0')
 	{
-		va_start(ap, format);
-		while (format[i] != '\0')
+		switch (format[i])
 		{
-			f = format[i];
-			if (i && (f == 'c' || f == 'i' || f == 'f' || f == 's'))
-				printf(", ");
-			switch (f)
-			{
-				case 'c':
-					putchar(va_arg(ap, int));
-					break;
-				case 'i':
-					printf("%d", va_arg(ap, int));
-					break;
-				case 'f':
+			case 'c':
+				printf("%c", va_arg(ap, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(ap, int));
+				break;
+			case 'f':
 				printf("%f", va_arg(ap, double));
-					break;
-				case 's':
-					s = va_arg(ap, char *);
-					if (!s)
-					{
-						printf("(nil)");
-						break;
-					}
-					printf("%s", s);
-					break;
-			}
-			i++;
+				break;
+			case 's':
+				s = va_arg(ap, char *);
+				if (!s)
+					s = "(nil)";
+				printf("%s", s);
+				break;
+			default:
+				skip = 1;
+				break;
 		}
-		printf("\n");
-		va_end(ap);
-		break;
+		if (!skip && format[i + 1] != '\0')
+			printf(", ");
+		i++;
+		skip = 0;
 	}
+	printf("\n");
+	va_end(ap);
 }
