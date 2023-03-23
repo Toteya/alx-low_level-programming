b803b5bc7a38a3ccc0b9f2ab8b704f5820d0fb84
Toteya <111172685+Toteya@users.noreply.github.com>
2023-03-23 17:05:12 +0000

    Task0: Sum variable number of integers

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+/* Libraries */
+#include <stdarg.h>
+
+/* Function Prototypes */
+int sum_them_all(const unsigned int n, ...);
+
+#endif
