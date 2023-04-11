ee8355f385cd5d6cfb2651d03923688e87d2265e
Toteya <111172685+Toteya@users.noreply.github.com>
2023-04-11 16:27:29 +0000

    Task2: get the bit of a number at a given index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+/**
+ * get_bit - Gets the bit of a number at the specified index
+ * @n: The number whose bit is to be found
+ * @index: The index of th ebit to be foud
+ *
+ * Return: The bit at the given index if successful.
+ * Otherwise return -1.
+ */
+int get_bit(unsigned int n, unsigned int index)
+{
+	while (index)
+	{
+		if (n <= 1 && index)
+			return (-1);
+		index--;
+		n = n >> 1;
+	}
+	n = n & 1;
+	return ((int) n);
+}
