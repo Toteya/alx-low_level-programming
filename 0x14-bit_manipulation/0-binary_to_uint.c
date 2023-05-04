2381e2c41a9907b496f2c6232586eefce7de6e08
Toteya <111172685+Toteya@users.noreply.github.com>
2023-05-04 12:10:38 +0000

    Task0: Convert binary to unsigned int)

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,23 +1,20 @@
-#include "main.h"
-
 /**
- * binary_to_uint - Converts a string representing binary number
- * to an unsigned int
- * @b: The binary number to be converted
+ * binary_to_uint - Converts a binary number to an unsigned integer
+ * @b: String of 0 and 1 chars, representing a binary number
  *
- * Return: The converted integer if successful, otherwise return 0.
+ * Return: The converted unsigned integer
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
 	int i;
+	unsigned int num = 0;
 
-	for (i = 0; b && b[i] != '\0'; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] < '0' || b[i] > '9')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		num = num << 1;
-		num += b[i] - '0';
+		num = num | (b[i] - '0');
 	}
 	return (num);
 }
