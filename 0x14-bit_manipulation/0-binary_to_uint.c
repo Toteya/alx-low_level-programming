ca78af183a271999aa2f7104c56a52bb8fcb4845
Toteya <111172685+Toteya@users.noreply.github.com>
2023-05-04 13:09:31 +0000

    Task0: Covert binary to unsigned integer, bug fix: null pointer

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,7 +9,7 @@ unsigned int binary_to_uint(const char *b)
 	int i;
 	unsigned int num = 0;
 
-	for (i = 0; b[i] != '\0'; i++)
+	for (i = 0; b && b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
