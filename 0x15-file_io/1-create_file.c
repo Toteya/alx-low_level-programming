88588e7e29da8c647b4991aac03c827bc302a44e
Toteya <111172685+Toteya@users.noreply.github.com>
2023-05-08 19:20:21 +0000

    Task1: Create a file with -rw------- permission, fix: Null string

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,10 +17,14 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
 	c = write(fd, text_content, strlen(text_content));
 	if (c == -1)
 		return (-1);
 	close(fd);
 	return (1);
 }
-
