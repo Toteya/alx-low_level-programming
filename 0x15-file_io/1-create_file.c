2ce94cf5bbd03e98b891d4fc88bf92a73bb42671
Toteya <111172685+Toteya@users.noreply.github.com>
2023-05-08 18:01:52 +0000

    Task1: Create a file with -rw------- permission

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file
+ * @filename: The file name
+ * @text_content: The content to be written to the file
+ *
+ * Return: 1 (SUCCESS), -1 (FAIL)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, c;
+
+	(void) text_content;
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	c = write(fd, text_content, strlen(text_content));
+	if (c == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
+
