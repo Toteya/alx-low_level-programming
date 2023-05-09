53e29e5cc96cb170f5028f3b2c3a1b00ecb3c767
Toteya <111172685+Toteya@users.noreply.github.com>
2023-05-09 15:19:18 +0000

    Task3: Copy content from one file to another, betty Fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,61 +9,64 @@
  */
 int main(int ac, char *av[])
 {
+	int c;
+
 	if (ac != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
 		return (97);
 	}
 
-	copy_file_content(av[1], av[2]);
+	c = copy_file_content(av[1], av[2]);
+	if (c == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		return (98);
+	}
+	else if (c == -2)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		return (98);
+	}
 	return (0);
 }
 
+/**
+ * copy_file_content - Copies the content of a file to another file
+ * @file_from: The file from which to copy
+ * @file_to: The file to which the content is to be copied
+ *
+ * Return: 1 on success. -1 (Failed to read), -2 (Failed to write)
+ */
 int copy_file_content(const char *file_from, const char *file_to)
 {
-	int fd1, fd2, r = 1, w, c;
+	int fd1, fd2, r = 1;
 	char buf;
 
 	fd1 = open(file_from, O_RDONLY);
 	if (fd1 == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
+		return (-1);
 	fd2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd2 == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", file_to);
-		exit(98);
-	}
-
+		return (-2);
 	while (r)
 	{
 		r = read(fd1, &buf, 1);
 		if (r == -1)
-		{
-			dprintf(2, "Error: Can't read from file %s\n", file_from);
-			exit(98);
-		}
+			return (-1);
 		if (!r)
 			break;
-		w = write(fd2, &buf, 1);
-		if (w == -1)
-		{
-			dprintf(2, "Error: Can't write to %s\n", file_to);
-			exit(98);
-		}
+		if (write(fd2, &buf, 1) == -1)
+			return (-2);
 	}
-	c = close(fd1);
-	if (c == -1)
+	if (close(fd1) == -1)
 	{
-		dprintf(2, "Can't clode fd %d\n", fd1);
+		dprintf(2, "Can't close fd %d\n", fd1);
 		exit(98);
 	}
-	c = close(fd2);
-	if (c == -1)
+	if (close(fd2) == -1)
 	{
-		dprintf(2, "Can't clode fd %d\n", fd2);
+		dprintf(2, "Can't close fd %d\n", fd2);
 		exit(98);
 	}
 	return (1);
