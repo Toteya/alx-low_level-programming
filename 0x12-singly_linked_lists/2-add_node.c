a51de0f9a27cf063d6db52c4471ae1bf606ee2da
Toteya <111172685+Toteya@users.noreply.github.com>
2023-05-29 13:32:11 +0000

    Task2: Add a new node to the beginning of a list, fixed: free all memory if malloc fails

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,11 +16,6 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	new->str = strdup(str);
-	if (str == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
 	new->len = strlen(new->str);
 	new->next = *head;
 	*head = new;
