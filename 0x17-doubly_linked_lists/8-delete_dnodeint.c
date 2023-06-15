357ed952814f928e9ac0c3fe34e1a584c2ebb09f
Toteya <111172685+Toteya@users.noreply.github.com>
2023-06-15 18:31:46 +0000

    Task8: Delete a node from a doubly linked list

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - Deletes a node at the given index of a doubly-
+ * linked list dlistint_t.
+ * @head: Pointer to the head node of the list
+ * @index: of the node to be deleted
+ *
+ * Return: 1 on success. 0 on failure.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *curr;
+
+	curr = *head;
+	while (curr && i <= index)
+	{
+		if (i == index)
+		{
+			if (curr->prev)
+				(curr->prev)->next = curr->next;
+			if (curr->next)
+				(curr->next)->prev = curr->prev;
+			if (!i)
+				*head = curr->next;
+			free(curr);
+			return (1);
+		}
+		curr = curr->next;
+		i++;
+	}
+	return (0);
+}
