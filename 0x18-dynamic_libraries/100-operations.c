4b8358547dd6790ccbd8755e858f2715e19a16ae
Toteya <111172685+Toteya@users.noreply.github.com>
2023-06-19 12:51:33 +0000

    Task2: Dynamic library that contains c functions

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,59 @@
+/**
+ * add - Calculates the sum of two numbers
+ * @a: The first integer to be added
+ * @b: The second integer to be added
+ *
+ * Return: The sum of a and b.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Calculates the difference of two integers
+ * @a: The number to be subtracted from
+ * @b: The number to subtract from a
+ *
+ * Return: The differenc of a and b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Calculates the product of two integers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: The product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Calculates the quotient of two integers
+ * @a: The numerator
+ * @b: The denominator
+ *
+ * Return: The quotient (tuncated int) of a over b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Calculates the remainder (modulus) of the division of two integers
+ * @a: The numerator
+ * @b: The denominator
+ *
+ * Return: The remainder of a over b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
