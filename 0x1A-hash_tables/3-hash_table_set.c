643630756b0642db19b4dfef8dc19ed376744d97
Toteya <111172685+Toteya@users.noreply.github.com>
2023-07-06 20:36:02 +0000

    Task3: Fixed case: key already exists -> overwrite existing value with new one

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+void free_hash_node(hash_node_t *hn);
+
 /**
  * hash_table_set - Adds an element to a hash_table
  * @ht: the hash table in which to add the element
@@ -12,38 +14,62 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *hn;
+	hash_node_t *hn, *p;
 	unsigned long int index;
 
-	if (!key || strcmp(key, "") == 0)
+	if (!key || strlen(key) == 0)
 		return (0);
-
 	hn = malloc(sizeof(hash_node_t));
 	if (!hn)
 		return (0);
-
 	hn->next = NULL;
 	hn->key = strdup(key);
 	if (!(hn->key))
 	{
-		free(hn);
+		free_hash_node(hn);
 		return (0);
 	}
 	hn->value = strdup(value);
 	if (!(hn->value))
 	{
-		free(hn->key);
-		free(hn);
+		free_hash_node(hn);
 		return (0);
 	}
-
 	index = key_index((unsigned char *)key, ht->size);
-
 	if (ht->array[index] != NULL)
 	{
+		p = ht->array[index];
+		while (p)
+		{
+			if (strcmp(key, p->key) == 0)
+			{ /* if the key already exists, overwrite the existing value*/
+				free(p->value);
+				p->value = strdup(value);
+				free_hash_node(hn);
+				return (1);
+			}
+			p = p->next;
+		}
 		hn->next = ht->array[index];
 	}
 	ht->array[index] = hn;
-
 	return (1);
+}
+
+/**
+ * free_hash_node - frees a hash_node_t
+ * @hn: the hash node element to be freed
+ *
+ * Return: Nothing.
+ */
+void free_hash_node(hash_node_t *hn)
+{
+	if (hn)
+	{
+		if (hn->key)
+			free(hn->key);
+		if (hn->value)
+			free(hn->value);
+		free(hn);
+	}
 }
