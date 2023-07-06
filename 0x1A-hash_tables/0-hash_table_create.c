801913dd23db88a388f500264601b9d9aa236f22
Toteya <111172685+Toteya@users.noreply.github.com>
2023-07-06 17:02:33 +0000

    Task0: Create a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_create - Creates a hash table
+ * @size: The size of the table (number of buckets)
+ *
+ * Return: Pointer to the hash_table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
+	if (!ht)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = calloc(size, sizeof(hash_node_t));
+	if (!(ht->array))
+		return (NULL);
+
+	return ht;
+}
