ba113bb15db4e3ca8b83b0f7266059f4aab96a5d
Toteya <111172685+Toteya@users.noreply.github.com>
2023-07-06 17:47:56 +0000

    Task2: Get index of a key

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Returns the index of the key. i.e. the index at which the node
+ * is to be stored in the array of the hash table
+ * @key: The key of the node
+ * @size: The size of the hash table array
+ *
+ * Return: The index of the key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index, hash;
+
+	hash = hash_djb2(key);
+	index = hash % size;
+
+	return index;
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,7 +1,8 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
-/* Data Structures */
+/* Structs */
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -34,5 +35,6 @@ typedef struct hash_table_s
 /* Function Prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
 #endif /* HASH_TABLES_H */
