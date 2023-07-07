fc386dad8f1c8c6047a0ea674cab4efa4fce0cf9
Toteya <111172685+Toteya@users.noreply.github.com>
2023-07-07 17:54:47 +0000

    Add an element to hash table: fixed edge case: value == NULL

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,11 +17,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *hn, *p;
 	unsigned long int index;
 
-	if (!key || !value || strlen(key) == 0)
+	if (!key || strlen(key) == 0)
 		return (0);
 	hn = malloc(sizeof(hash_node_t));
 	if (!hn)
 		return (0);
+
+	if (value == NULL)
+		value = "";
 
 	hn->next = NULL;
 	hn->key = strdup(key);
