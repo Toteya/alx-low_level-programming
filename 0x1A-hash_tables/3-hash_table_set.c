788d3443747e1835c93a21217da78bf7f2de67aa
Toteya <111172685+Toteya@users.noreply.github.com>
2023-07-08 14:30:49 +0000

    Task3: fixed case - hash table is NULL, betty fixed

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,7 +20,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!ht || !key || strlen(key) == 0)
 		return (0);
-	
+
 	hn = malloc(sizeof(hash_node_t));
 	if (!hn)
 		return (0);
