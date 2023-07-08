3c879d1d59b6aab080cafefe11cb1486acdc85ac
Toteya <111172685+Toteya@users.noreply.github.com>
2023-07-08 14:27:06 +0000

    Task3: fixed case - hash table is NULL

--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -14,5 +14,12 @@ int main(void)
 
 	ht = hash_table_create(1024);
 	hash_table_set(ht, "betty", "cool");
+	hash_table_set(ht, "betty", NULL);
+
+/*
+	hash_table_set(ht, "subgenera", "50 cent");
+	hash_table_set(ht, "stylist", "many men");
+*/	
+	hash_table_delete(ht);
 	return (EXIT_SUCCESS);
 }
